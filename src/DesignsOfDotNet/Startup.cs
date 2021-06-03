7b87c688520a4be1408f7fad9fa44017f15adb89
Immo Landwerth <immol@microsoft.com>
2021-06-03 13:29:27 -0700

    Remove boilerplate

--- src/DesignsOfDotNet/Startup.cs
+++ src/DesignsOfDotNet/Startup.cs
@@ -17,8 +17,6 @@ namespace DesignsOfDotNet
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -31,7 +29,6 @@ namespace DesignsOfDotNet
             services.AddSingleton<GitHubClientFactory>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +38,6 @@ namespace DesignsOfDotNet
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
